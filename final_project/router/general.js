0575aae648bfaf60efdcbc793aa4f05f7f2c8fa0
rabbitDLaLune <dearkai311@gmail.com>
2026-06-11 00:57:49 +0800

    Refactor internal routes and update comments

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,25 +36,27 @@ public_users.post("/register", (req, res) => {
 });
 
 
-// Internal route for Axios - all books
+// Internal route used by Axios for Task 10
 public_users.get('/internal/books', function (req, res) {
   return res.status(200).json(books);
 });
 
 
-// Internal route for Axios - ISBN
+// Internal route used by Axios for Task 11
 public_users.get('/internal/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
   if (books[isbn]) {
     return res.status(200).json(books[isbn]);
   } else {
-    return res.status(404).json({ message: "Book not found" });
+    return res.status(404).json({
+      message: "Book not found"
+    });
   }
 });
 
 
-// Internal route for Axios - author
+// Internal route used by Axios for Task 12
 public_users.get('/internal/author/:author', function (req, res) {
   const author = req.params.author;
   let matchingBooks = {};
@@ -68,12 +70,14 @@ public_users.get('/internal/author/:author', function (req, res) {
   if (Object.keys(matchingBooks).length > 0) {
     return res.status(200).json(matchingBooks);
   } else {
-    return res.status(404).json({ message: "No books found by this author" });
+    return res.status(404).json({
+      message: "No books found by this author"
+    });
   }
 });
 
 
-// Internal route for Axios - title
+// Internal route used by Axios for Task 13
 public_users.get('/internal/title/:title', function (req, res) {
   const title = req.params.title;
   let matchingBooks = {};
@@ -87,23 +91,27 @@ public_users.get('/internal/title/:title', function (req, res) {
   if (Object.keys(matchingBooks).length > 0) {
     return res.status(200).json(matchingBooks);
   } else {
-    return res.status(404).json({ message: "No books found with this title" });
+    return res.status(404).json({
+      message: "No books found with this title"
+    });
   }
 });
 
 
-// Task 10: Get the book list available in the shop using Axios with async-await
+// Task 10: Get the book list available in the shop using async-await with Axios
 public_users.get('/', async function (req, res) {
   try {
     const response = await axios.get('http://localhost:5000/internal/books');
     return res.status(200).send(JSON.stringify(response.data, null, 4));
   } catch (error) {
-    return res.status(500).json({ message: "Error retrieving books" });
+    return res.status(500).json({
+      message: "Error retrieving books"
+    });
   }
 });
 
 
-// Task 11: Get book details based on ISBN using Axios with async-await
+// Task 11: Get book details based on ISBN using async-await with Axios
 public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
 
@@ -111,12 +119,14 @@ public_users.get('/isbn/:isbn', async function (req, res) {
     const response = await axios.get(`http://localhost:5000/internal/isbn/${isbn}`);
     return res.status(200).send(JSON.stringify(response.data, null, 4));
   } catch (error) {
-    return res.status(404).json({ message: "Book not found" });
+    return res.status(404).json({
+      message: "Book not found"
+    });
   }
 });
 
 
-// Task 12: Get book details based on author using Axios with Promise callbacks
+// Task 12: Get book details based on Author using Promise callbacks with Axios
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
 
@@ -125,12 +135,14 @@ public_users.get('/author/:author', function (req, res) {
       return res.status(200).send(JSON.stringify(response.data, null, 4));
     })
     .catch((error) => {
-      return res.status(404).json({ message: "No books found by this author" });
+      return res.status(404).json({
+        message: "No books found by this author"
+      });
     });
 });
 
 
-// Task 13: Get book details based on title using Axios with Promise callbacks
+// Task 13: Get book details based on Title using Promise callbacks with Axios
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
 
@@ -139,7 +151,9 @@ public_users.get('/title/:title', function (req, res) {
       return res.status(200).send(JSON.stringify(response.data, null, 4));
     })
     .catch((error) => {
-      return res.status(404).json({ message: "No books found with this title" });
+      return res.status(404).json({
+        message: "No books found with this title"
+      });
     });
 });
 
@@ -151,7 +165,9 @@ public_users.get('/review/:isbn', function (req, res) {
   if (books[isbn]) {
     return res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
   } else {
-    return res.status(404).json({ message: "Book not found" });
+    return res.status(404).json({
+      message: "Book not found"
+    });
   }
 });
 
